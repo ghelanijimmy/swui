5da3e9c72c3b2daa88969f8f291037b772848d43
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2018-11-13 11:20:38 -0500

    js updates read more to hide to read more change

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ function hideShow() {
         hiddenText.style.opacity = "0";
         setTimeout(function(){
             hiddenText.style.height = "0";
+            document.getElementById('changeText').innerHTML = 'Read More <i class="fa fa-chevron-down" aria-hidden="true"></i>';
         }, 200);
     } else {
         hiddenText.style.opacity = "1";
         hiddenText.style.height = "auto";
+        document.getElementById('changeText').innerHTML = 'Hide <i class="fa fa-chevron-up" aria-hidden="true"></i>';
     }
 }
